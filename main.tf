7a96c00bb4df2c6dc6ae10049d691667fe987584
akhidev <59053561+akhidev@users.noreply.github.com>
2024-05-15 13:30:11 +0100

    updated iac

--- main.tf
+++ main.tf
@@ -12,6 +12,23 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam-role" {
+  name               = "iam-role-lambda-api-gateway"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
 
 resource "aws_lambda_function" "lambda-function" {
   filename      = "${path.module}/src"
