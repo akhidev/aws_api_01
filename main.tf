df2cb54684b7834faed8a6398fa2e2c25a8dec03
akhidev <59053561+akhidev@users.noreply.github.com>
2024-05-15 13:46:46 +0100

    updated

--- main.tf
+++ main.tf
@@ -12,6 +12,8 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_caller_identity" "current" {}
+
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
@@ -30,10 +32,94 @@ resource "aws_iam_role" "iam-role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+resource "aws_iam_role_policy" "iam-policy" {
+  name   = "cloudwatch-policy"
+  role   = aws_iam_role.iam-role.id
+  policy = file("${path.module}/iam-policy.json")
+}
+
 resource "aws_lambda_function" "lambda-function" {
   filename      = "${path.module}/src.zip"
   function_name = var.lambda_name
   role          = aws_iam_role.iam-role.arn
   handler       = "src.lambda_handler"
   runtime       = "python3.9"
+}
+
+resource "aws_api_gateway_rest_api" "API" {
+  name        = "lambda-api"
+  description = "lambda-api"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "Resource" {
+  rest_api_id = aws_api_gateway_rest_api.API.id
+  parent_id   = aws_api_gateway_rest_api.API.root_resource_id
+  path_part   = "verify-json"
+}
+
+resource "aws_api_gateway_method" "Method" {
+  rest_api_id   = aws_api_gateway_rest_api.API.id
+  resource_id   = aws_api_gateway_resource.Resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "Integration" {
+  rest_api_id             = aws_api_gateway_rest_api.API.id
+  resource_id             = aws_api_gateway_resource.Resource.id
+  http_method             = aws_api_gateway_method.Method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda-function.invoke_arn
+}
+
+
+resource "aws_lambda_permission" "apigw-lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda-function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.aws_region}:${aws_caller_identity.current.id}:${aws_api_gateway_rest_api.API.id}/*/${aws_api_gateway_method.Method.http_method}${aws_api_gateway_resource.Resource.path}"
+}
+
+
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.API.id
+  resource_id = aws_api_gateway_resource.Resource.id
+  http_method = aws_api_gateway_method.Method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "Integration-Response" {
+  rest_api_id = aws_api_gateway_rest_api.API.id
+  resource_id = aws_api_gateway_resource.Resource.id
+  http_method = aws_api_gateway_method.Method.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+
+  depends_on = [
+    aws_api_gateway_integration.Integration
+  ]
+
+  response_templates = {
+    "application/xml" = <<EOF
+#set($inputRoot = $input.path('$'))
+<?xml version="1.0" encoding="UTF-8"?>
+<message>
+    $inputRoot.body
+</message>
+EOF
+  }
+}
+
+
+resource "aws_api_gateway_deployment" "example" {
+  depends_on = [
+    aws_api_gateway_integration.Integration
+  ]
+  rest_api_id = aws_api_gateway_rest_api.API.id
+  stage_name  = "test"
 }
